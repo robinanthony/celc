cb49cb145fec7d757d843d24120703be6646a729
Robin Anthony <anthony.robin1@etu.univ-orleans.fr>
2019-11-07 11:38:32 +0100

    Modification du .gitignore afin d'ignorer les fichiers __pycache__. Gestion du cas où le dossier target n'existe pas.

--- bdd/run.py
+++ bdd/run.py
@@ -2,10 +2,13 @@ print("----------- DEBUT DU PROGRAMME -----------")
 
 import json
 import re
+import os
 
 import resources.scripts_bdd.arrets_tao as arrets_tao
 import resources.scripts_bdd.lignes_tao as lignes_tao
 import resources.scripts_bdd.stations_velo as stations_velo
+
+os.makedirs("target", exist_ok=True)
 
 arrets_tao.traitement()
 lignes_tao.traitement()

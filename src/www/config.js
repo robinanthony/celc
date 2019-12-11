c465c6790b28d82bb5ae7e682bf95ea8914ca832
adrien010407 <adbou45@hotmail.fr>
2019-12-11 17:05:39 +0100

    remise des vals defaut de config.js

--- src/www/config.js
+++ src/www/config.js
@@ -1,10 +1,10 @@
 var geoserver = {
     // host : "192.168.46.196",
-    host : "192.168.1.19",
+    host : "localhost",
     port : "8081"
 }
 
 var api = {
-    host : "192.168.1.19",
+    host : "localhost",
     port : "5050"
 }
